18f5ee3e8bd012dddcf14a1cb8cccfd2c9d53192
林泽锋 <282245889@qq.com>
2017-11-22 10:31:28 +0800

    用户模块查看积分明细

--- src/main/java/com/dinner/snqjf/back/controller/MobileUserController.java
+++ src/main/java/com/dinner/snqjf/back/controller/MobileUserController.java
@@ -2,6 +2,8 @@ package com.dinner.snqjf.back.controller;
 
 import javax.inject.Inject;
 
+import org.springframework.http.MediaType;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 import com.dinner.snqjf.common.base.controller.GenericController;
@@ -10,6 +12,7 @@ import com.dinner.snqjf.common.base.service.GenericService;
 import com.dinner.snqjf.back.entity.MobileUser;
 import com.dinner.snqjf.back.entity.QueryMobileUser;
 import com.dinner.snqjf.back.service.MobileUserService;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *@author linzf
@@ -22,5 +25,18 @@ public class MobileUserController extends GenericController<MobileUser, QueryMob
 	@Override
 	protected GenericService<MobileUser, QueryMobileUser> getService() {
 		return mobileUserService;
+	}
+
+	/**
+	 * 功能描述：跳转到查看积分明细的页面
+	 * @param entity
+	 * @param model
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/toUserScoreDetailList",method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+	public String toUserScoreDetailList(MobileUser entity,Model model) throws Exception {
+		model.addAttribute("entity",entity);
+		return getPageBaseRoot()+"/userScoreDetailList";
 	}
 }
