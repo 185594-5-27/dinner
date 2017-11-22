dcad42e46d2aab3396e2d3705671335615e51cb0
林泽锋 <282245889@qq.com>
2017-11-22 14:49:02 +0800

    移动端API接口开发工作

---
+++ src/main/java/com/dinner/snqjf/api/entity/MobileUserLogin.java
@@ -0,0 +1,30 @@
+package com.dinner.snqjf.api.entity;
+
+/*
+* 类描述：用户移动端登陆实体
+* @auther linzf
+* @create 2017/11/22 0022 
+*/
+public class MobileUserLogin {
+
+    //名字
+    private String name;
+    //电话
+    private String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
--- src/main/java/com/dinner/snqjf/api/user/UserAPI.java
+++ src/main/java/com/dinner/snqjf/api/user/UserAPI.java
@@ -1,8 +1,15 @@
 package com.dinner.snqjf.api.user;
 
+import com.dinner.snqjf.api.entity.MobileUserLogin;
+import com.dinner.snqjf.back.service.MobileUserService;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.inject.Inject;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,11 +17,16 @@ import java.util.Map;
 @RequestMapping("/api/user")
 public class UserAPI {
 
+    @Inject
+    private MobileUserService mobileUserService;
+
     /**
      * 功能描述：实现移动端客户登陆的接口
      * @return
      */
-    public Map<String,Object> userLogin(){
+    @RequestMapping(value = "/userLogin",method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Map<String,Object> userLogin(MobileUserLogin entity){
         Map<String,Object> result = new HashMap<String, Object>();
 
         return result;
